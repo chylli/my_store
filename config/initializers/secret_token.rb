3ac9b8ed2152ed3edd7aeda743f89c2ee65f502f
Chylli <chylli.email@gmail.com>
2013-09-09 16:45:11 +0800

    rails new my_store -T -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStore::Application.config.secret_token = '0332182aacdc75fdf303b44b45fd548501f2b66d2920d1b0eee5e33b89423163fa8b78b8602f887903398b09db14ee22583d1d700425872d1ffe2f7d0cdb856f'
